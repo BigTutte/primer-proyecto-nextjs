b702c167f82481bbdf41f8389103a10169d682d4
Matteo Cardozo <matteo.gomez@fing.edu.uy>
2025-02-23 11:39:56 -0300

    Creacion del proyecto

---
+++ proyectito/app/product-detail/page.tsx
@@ -0,0 +1,3 @@
+export default function ProductDetail() {
+    return <h1>Detalle del producto</h1>
+}
---
+++ proyectito/app/products/page.tsx
@@ -0,0 +1,3 @@
+export default function ProductsPage() { // Componente de la página de productos
+    return <h1>Productos</h1>
+}
